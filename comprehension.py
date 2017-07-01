9d39c64b65188b2e7aec7e87e077845a1141b4ff
mateszummer <szummerm@gmail.com>
2017-07-01 12:27:59 +0200

    completed

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,33 @@
-import random
+import random #import random, enables you to use its modules
 
-guessesTaken = 0
+guessesTaken = 0 #assigns 0 to guessesTaken variable
 
-print('Hello! What is your name?')
-myName = input()
+print('Hello! What is your name?') #prints out whats inside the ()
+myName = input() # assigns input to myName variable
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+number = random.randint(1, 20) #assigns random intiger between 1(lowest value it can be) and 19(highest value it van be) to number variable
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.') #prints out "well, (the value of myName variable) , I am thinking..."
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+while guessesTaken < 6: #repeats syntaxes inside the loop until guessesTaken value is lower than 6.
+    print('Take a guess.') #prints...
+    guess = input() # assigns input to guess variable
+    guess = int(guess) #transforms guess variable to an intiger variable
 
-    guessesTaken += 1
+    guessesTaken += 1 # assaigns guessesTaken +1 to guessesTaken variable
 
-    if guess < number:
-        print('Your guess is too low.')
+    if guess < number: # runs syntaxes inside, in case guess is lower than number
+        print('Your guess is too low.') # prints ...
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess > number: # runs syntaxes inside, in case guess is higher than number
+        print('Your guess is too high.') # prints
 
-    if guess == number:
-        break
+    if guess == number: # runs syntaxes inside, in case guess is equal to number
+        break #quits the loop, lines after this in the loop wont be "played"
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+if guess == number: # runs syntaxes inside in case guess equal to number
+    guessesTaken = str(guessesTaken) # transforms guessesTaken to a string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!') # prints...
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess != number: # runs syntaxes inside in case guess is not equal to number
+    number = str(number) # transforms number to string
+    print('Nope. The number I was thinking of was ' + number) # prints...
